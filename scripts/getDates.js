2b5e92a9c4a587e1d3a4f2d70bb990a2808cf148
JaronrHill <161978628+JaronrHill@users.noreply.github.com>
2024-03-14 01:05:20 -0600

    home page update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,3 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("lastUpdated").textContent += document.lastModified;
 });
 
+document.querySelector('.dark-mode-toggle').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+});
